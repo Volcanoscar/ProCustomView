7a6a09e5f02f51ec07ee65477350ad590debad21
QichaoChen <2009chenqc@163.com>
2015-03-17 00:43:07 +0800

    add More coloreffect

--- app/src/main/java/me/chenqichao/procustomview/view/view2/ColorFilterView2.java
+++ app/src/main/java/me/chenqichao/procustomview/view/view2/ColorFilterView2.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -22,6 +24,7 @@ public class ColorFilterView2 extends View {
     private Paint mPaint;
     private Bitmap bitmap;
     private int x, y;
+    private int currentEffectId = 0;
 
     //一般会这样写自定义View的初始化函数,当然不是绝
     //动态创建View时调用，例如在Activity中使用这样代码：SimpleCustomView scv = new SimpleCustomView(context)
@@ -99,6 +102,107 @@ public class ColorFilterView2 extends View {
     }
 
     public void ShowNextEffect() {
+        currentEffectId++;
+        switch (currentEffectId) {
+            case 1:
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix1()));
+                invalidate();
+                break;
+            case 2:
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix2()));
+                invalidate();
+                break;
+            case 3:
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix3()));
+                invalidate();
+                break;
+            case 4:
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix4()));
+                invalidate();
+                break;
+            case 5:
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix5()));
+                invalidate();
+                break;
+            case 6:
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix6()));
+                invalidate();
+                break;
+            case 7:
+                currentEffectId = 0;
+                mPaint.setColorFilter(new ColorMatrixColorFilter(getColorMatrix7()));
+                invalidate();
+                break;
+        }
+    }
 
+    //变暗
+    private ColorMatrix getColorMatrix1() {
+        return new ColorMatrix(new float[]{
+                0.5f, 0, 0, 0, 0,
+                0, 0.5f, 0, 0, 0,
+                0, 0, 0.5f, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+    }
+
+    //灰度化
+    private ColorMatrix getColorMatrix2() {
+        return new ColorMatrix(new float[]{
+                0.33f, 0.59f, 0.11f, 0, 0,
+                0.33f, 0.59f, 0.11f, 0, 0,
+                0.33f, 0.59f, 0.11f, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+    }
+
+    //反相
+    private ColorMatrix getColorMatrix3() {
+        return new ColorMatrix(new float[]{
+                -1, 0, 0, 1, 1,
+                0, -1, 0, 1, 1,
+                0, 0, -1, 1, 1,
+                0, 0, 0, 1, 0,
+        });
+    }
+
+    //RGB->BRG
+    private ColorMatrix getColorMatrix4() {
+        return new ColorMatrix(new float[]{
+                0, 0, 1, 0, 0,
+                0, 1, 0, 0, 0,
+                1, 0, 0, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+    }
+
+    //老照片
+    private ColorMatrix getColorMatrix5() {
+        return new ColorMatrix(new float[]{
+                0.393f, 0.769f, 0.189f, 0, 0,
+                0.349f, 0.686f, 0.168f, 0, 0,
+                0.272f, 0.534f, 0.131f, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+    }
+
+    //去色高对比
+    private ColorMatrix getColorMatrix6() {
+        return new ColorMatrix(new float[]{
+                1.5f, 1.5f, 1.5f, 0, -1,
+                1.5f, 1.5f, 1.5f, 0, -1,
+                1.5f, 1.5f, 1.5f, 0, -1,
+                0, 0, 0, 1, 0,
+        });
+    }
+
+    //饱和对比度加强
+    private ColorMatrix getColorMatrix7() {
+        return new ColorMatrix(new float[]{
+                1.438f, -0.122f, -0.016f, 0, -0.03f,
+                -0.062f, 1.378f, -0.016f, 0, 0.05f,
+                -0.062f, -0.122f, 1.483f, 0, -0.02f,
+                0, 0, 0, 1, 0,
+        });
     }
 }
